59d9a955f64cb7b0ffccb3e3167395165997e3c4
Ozik14 <98962035+Ozik14@users.noreply.github.com>
2022-03-18 20:53:01 +0300

    Task_bars1

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_bars1
+{
+    public class Test
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            var el = Console.ReadKey();
+            while (true)
+            {
+                if (el.Key ==ConsoleKey.C)
+                {
+                    break;
+                }
+                else
+                {
+                    OnKeyPressed?.Invoke(this, el.KeyChar);
+                }
+
+            }
+           
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sub = new Program();
+            sub.Method();
+        }
+
+
+        public void Method()
+        {
+            var el = new Test();
+
+            el.OnKeyPressed += InputChar;
+
+            el.Run();
+        }
+         
+        public void InputChar(object sender, char letter)
+        {
+            Console.WriteLine(letter);
+        }
+       
+
+
+
+
+
+
+    }
+}
