230663e874c58d76a937a1d13551ddc81417fd35
Ozik14 <98962035+Ozik14@users.noreply.github.com>
2022-03-29 16:42:46 +0300

    CollectionAndGeneric
    BARS Collection and Generic

---
+++ Generic.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace Generic;
+
+interface ILogger
+{
+    public void LogInfo(string message);
+    public void LogWarning(string message);
+    public void LogError(string message, Exception ex);
+}
+
+public class LocalFileLogger<T> : ILogger
+{
+    private string FilePath;
+
+    public LocalFileLogger(string fileLocation)
+    {
+        this.FilePath = fileLocation;
+    }
+
+    public void LogInfo(string message)
+    {
+        File.AppendAllText(FilePath, $"[Info]: [{typeof(T).Name}] : {message}" + '\n');
+    }
+
+    public void LogWarning(string message)
+    {
+        File.AppendAllText(FilePath, $"[Warning] : [{typeof(T).Name}] : {message}" + '\n');
+    }
+
+    public void LogError(string message, Exception ex)
+    {
+        File.AppendAllText(FilePath, $"[Error] : [{typeof(T).Name}] : {message}. {ex.Message}" + '\n');
+    }
+}
+
+public class People
+{
+    public int ID { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
+
+class Program
+{
+   
+    public static void Main(string[] args)
+    {
+        string Test = ".\\test.txt";
+
+        var intLogger = new LocalFileLogger<int>(Test);
+        intLogger.LogInfo("LogInfo int!");
+        intLogger.LogWarning("LogWarning int!");
+        intLogger.LogError("LogError int!",
+            new Exception("Exception int!"));
+
+        var stringLogger = new LocalFileLogger<string>(Test);
+        stringLogger.LogInfo("LogInfo string!");
+        stringLogger.LogWarning("LogWarning string!");
+        stringLogger.LogError("LogError string!",
+            new Exception("Exception string test text"));
+
+        var studentLogger = new LocalFileLogger<People>(Test);
+        studentLogger.LogInfo("LogInfo People!");
+        studentLogger.LogWarning("LogWarning People!");
+        studentLogger.LogError("LogError People!",
+            new Exception("Exception People test text"));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,61 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace task_bars1
+namespace Collection;
+public class Entity
 {
-    public class Test
+    public int Id { get; set; }
+    public int ParentId { get; set; }
+
+    public string? Name { get; set; }
+
+    public Entity(int Id, int ParentId, string Name)
     {
-        public event EventHandler<char> OnKeyPressed;
-
-        public void Run()
-        {
-            var el = Console.ReadKey();
-            while (true)
-            {
-                if (el.Key ==ConsoleKey.C)
-                {
-                    break;
-                }
-                else
-                {
-                    OnKeyPressed?.Invoke(this, el.KeyChar);
-                }
-
-            }
-           
-        }
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            var sub = new Program();
-            sub.Method();
-        }
-
-
-        public void Method()
-        {
-            var el = new Test();
-
-            el.OnKeyPressed += InputChar;
-
-            el.Run();
-        }
-         
-        public void InputChar(object sender, char letter)
-        {
-            Console.WriteLine(letter);
-        }
-       
-
-
-
-
-
-
+        this.Id = Id;
+        this.ParentId = ParentId;
+        this.Name = Name;
     }
 }
+
+  static class Program
+{
+    public static Dictionary<int, List<Entity>> ConvertToDict(List<Entity> list)
+    {
+        Dictionary<int, List<Entity>> newDict = new Dictionary<int, List<Entity>>();
+
+        List<Entity> entities = new List<Entity>();
+
+        foreach (Entity entity in list)
+        {
+            if (newDict.TryGetValue(entity.ParentId, out List<Entity> ?entitiesParent))
+            {
+                if (!entitiesParent.Contains(entity))
+                {
+                    entitiesParent.Add(entity);
+                }
+            }
+
+            else
+            {
+                newDict.Add(entity.ParentId, new List<Entity>() { entity });
+            }
+
+        }
+        return newDict;
+    }
+    private static void Main()
+    {
+
+        Entity RootEntity = new Entity(1, 0, "Root entity");
+        Entity Ent1 = new Entity(2, 1, "Child of 1 entity");
+        Entity Ent2 = new Entity(3, 1, "Child of 1 entity");
+        Entity Ent3 = new Entity(4, 2, "Child of 2 entity");
+        Entity Ent4 = new Entity(5, 4, "Child of 4 entity");
+
+        List<Entity> entList = new List<Entity>() { RootEntity, Ent1, Ent2, Ent3, Ent4 };
+
+        Dictionary<int, List<Entity>> entDict = ConvertToDict(entList);
+
+        foreach (var item in entList)
+        {
+            Console.WriteLine($"Entity{{Id = {item.Id}, ParentId = {item.ParentId}, Name = {item.Name}}}");
+        }
+
+        Console.WriteLine();
+
+        foreach (var keys in entDict)
+        {
+            Console.Write($"Key : {keys.Key}, Value: List{{");
+            foreach (var entities in keys.Value)
+            {
+                Console.Write($"Entity{{Id = {entities.Id}}} ");
+            }
+            Console.WriteLine("}");
+        }
+    }
+
+   
+}
+
+
+
+
+
+
+
